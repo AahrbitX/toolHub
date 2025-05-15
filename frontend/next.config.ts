449faf0581b5c2a5f423318e4bd4cc1ee9283ead
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2025-05-15 21:55:34 +0530

    hotfix: Deploy Pagenotfound Problem?

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  // add other config options here
 };
 
 export default nextConfig;
+
